50edd9e9f6ac4d983c3bcb4d2a49fc211c26aa5e
unknown <kapilpremad0@gmail.com>
2025-09-09 16:16:17 +0530

    first commit

---
+++ routes/admin/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// Admin subroutes
+router.use('/', require('../admin/homeRoutes'));
+
+router.use('/users', require('../admin/userRoutes'));
+
+module.exports = router;
